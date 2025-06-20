56f90dda5feb387a1e59257dd3918ae0f8ab8b71
Jarosław Gosławski <jarek93pl@hotmail.com>
2025-06-20 20:08:28 +0200

    Add limit TIme

--- Program.cs
+++ Program.cs
@@ -1,23 +1,34 @@
 ﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Dai;
 using Emgu.CV.Structure;
 using System;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 public class FrameReader
 {
     static string textName = "frame_";
-    public static void ReadFrames(string videoPath, int perFrame = 1)
+    public static void ReadFrames(string videoPath, int perFrame = 1, double startTime = 0, double endTime = 0)
     {
         try
         {
             using (var capture = new VideoCapture(videoPath))
             {
+                bool usFpsLimit = false;
+                int fpsToRead = Int32.MaxValue;
+                if (endTime > startTime)
+                {
+                    double fps = capture.Get(Emgu.CV.CvEnum.CapProp.Fps);
+                    fpsToRead = Convert.ToInt32((fps * (endTime - startTime)));
+                    usFpsLimit = true;
+                }
+                capture.Set(CapProp.PosMsec, 1000 * startTime);
                 if (capture.IsOpened)
                 {
                     Mat frame = new Mat();
                     int frameCount = 0;
-
                     while (capture.Read(frame))
                     {
                         // Process the frame (e.g., display it, save it)
@@ -34,6 +45,10 @@ public class FrameReader
                             }
                         }
                         frameCount++;
+                        if (usFpsLimit && frameCount > fpsToRead)
+                        {
+                            break;
+                        }
                     }
                     Console.WriteLine($"Read {frameCount} frames.");
                 }
@@ -58,13 +73,16 @@ public class FrameReader
 
         if (args == null || args.Length == 0)
         {
-            Console.WriteLine("give a path and, per frame you want get");
+            Console.WriteLine("give a path and, per frame you want get, start , end time");
         }
         else
         {
 
             int perFrame = args.Length > 1 ? Convert.ToInt32(args[1]) : 0;
-            ReadFrames(args[0], perFrame);
+            double startTime = args.Length > 2 ? double.Parse(args[2], CultureInfo.InvariantCulture) : 0;
+            double endTime = args.Length > 2 ? double.Parse(args[3], CultureInfo.InvariantCulture) : 0;
+
+            ReadFrames(args[0], perFrame, startTime, endTime);
         }
     }
 }
