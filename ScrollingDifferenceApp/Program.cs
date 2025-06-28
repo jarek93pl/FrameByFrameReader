ba1ae2fb747ae410d6a919b0b48845d8a74b2402
Jarosław Gosławski <jarek93pl@hotmail.com>
2025-06-28 03:23:07 +0200

    Add app to cach scroliong

---
+++ ScrollingDifferenceApp/Program.cs
@@ -0,0 +1,237 @@
+﻿//#define difrence
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+public class ScrollingDifferenceApp
+{
+#if difrence
+
+    static int indexDifrenceImage = 0;
+    static nint ToTestDifrencesnInt;
+#endif
+    static int Width;
+    static int Height;
+    class RowResult
+    {
+        public string path;
+        public int Number, Gap;
+    }
+    static string textName = "frame_";
+
+    const string parameterForBinaryMask = "-binaryMask";
+    const string parameterFormaxMove = "-maxMove";
+    public unsafe static void Main(string[] args)
+    {
+        int NumberRowToProces;
+        List<RowResult> rowResults = new List<RowResult>();
+        Console.WriteLine($"arrey path frames, {parameterForBinaryMask} ");
+        if (args == null || args.Length == 0)
+        {
+            textName = "FrameLoadFromFile_";
+            args = File.ReadAllLines("args.txt");
+        }
+
+        if (args == null || args.Length == 0)
+        {
+            Console.WriteLine("give proper parameters or files named args.txt");
+        }
+        else
+        {
+            string maskPath = null;
+            string[] imagesPath;
+
+            imagesPath = LoadParameters(args, ref maskPath, out float MaxMove, out rowResults);
+            //variables
+            bool* maskImageBool = (bool*)0;
+            byte* OldImageByte, NewImageByte;
+            bool UseMask = false;
+
+
+            //loading images
+            if (maskPath != null)
+            {
+                Bitmap MaskImage = (Bitmap)Bitmap.FromFile(maskPath);
+                maskImageBool = (bool*)StreamWorking.LoadMonohrome(MaskImage);
+                MaskImage.Dispose();
+                UseMask = true;
+            }
+
+            {
+                Bitmap OldImage = (Bitmap)Bitmap.FromFile(imagesPath[0]);
+                Width = OldImage.Width;
+                Height = OldImage.Height;
+
+#if difrence
+                ToTestDifrencesnInt = Marshal.AllocHGlobal(Width * Height);
+#endif
+                OldImageByte = StreamWorking.LoadMonohrome(OldImage);
+                StreamWorking.OtshuMethod(Width * Height, OldImageByte);
+                OldImage.Dispose();
+                NumberRowToProces = (int)(MaxMove * Height);
+
+            }
+            ///
+            int Posytion = 0;
+            for (int i = 1; i < imagesPath.Length; i++)
+            {
+                NewImageByte = LoadPointer(imagesPath, i);
+
+                decimal[] results = new decimal[NumberRowToProces];
+
+
+#if difrence 
+for (int item = 1; item < NumberRowToProces; item++)
+                {
+#else
+                Parallel.ForEach(Enumerable.Range(0, NumberRowToProces ), item =>{
+#endif
+                    int numberRowToProces = (Height - item);
+                    int sift = (item * Width);
+                    results[item] = GetDifrent(OldImageByte + sift, NewImageByte, UseMask, maskImageBool + sift, maskImageBool, (Width * numberRowToProces));
+
+
+                }
+
+                );
+                rowResults[i].Gap = Posytion += MinIndex(results);
+                Marshal.FreeHGlobal((nint)OldImageByte);
+                OldImageByte = NewImageByte;
+            }
+        }
+        SaveResult(rowResults);
+
+    }
+
+    private static void SaveResult(List<RowResult> rowResults)
+    {
+        List<string> strings = new List<string>(rowResults.Count + 1);
+        strings.Add("index,path,gap,mask");
+        foreach (var item in rowResults)
+        {
+            strings.Add($"{item.Number},{item.path},{item.Gap},");
+        }
+        File.WriteAllLines("output.csv", strings.ToArray());
+    }
+
+    public static int MinIndex(decimal[] table)
+    {
+        int minIndex = 0;
+        decimal minValue = decimal.MaxValue;
+        for (int i = 1; i < table.Length; i++)
+        {
+            decimal current = table[i];
+            if (minValue > current)
+            {
+                minIndex = i;
+                minValue = current;
+            }
+        }
+        return minIndex;
+    }
+
+    private static unsafe string[] LoadParameters(string[] args, ref string maskPath, out float maxMove, out List<RowResult> rowResults)
+    {
+        rowResults = new List<RowResult>(args.Length);
+        string[] imagesPath;
+        int indexMask = Array.IndexOf(args, parameterForBinaryMask);
+        int indexMove = Array.IndexOf(args, parameterFormaxMove);
+        if (indexMask != -1)
+        {
+            maskPath = args.Skip(indexMask + 1).First();
+        }
+        if (indexMove != -1)
+        {
+            maxMove = float.Parse(args.Skip(indexMove + 1).First(), CultureInfo.InvariantCulture);
+        }
+        else
+        {
+            maxMove = 1;
+        }
+
+        int numberOfPaths = new int[] { indexMove, indexMask }.Min();
+
+        if (numberOfPaths == -1)
+        {
+            imagesPath = args;
+        }
+        else
+        {
+            imagesPath = args.Take(indexMask).ToArray();
+        }
+
+        for (int i = 0; i < numberOfPaths; i++)
+        {
+            rowResults.Add(new RowResult() { path = args[i], Number = i });
+        }
+        return imagesPath;
+    }
+
+    private static unsafe byte* LoadPointer(string[] imagesPath, int i)
+    {
+        byte* NewImageByte;
+        Bitmap Image = (Bitmap)Bitmap.FromFile(imagesPath[i]);
+        NewImageByte = StreamWorking.LoadMonohrome(Image);
+
+        StreamWorking.OtshuMethod(Width * Height, NewImageByte);
+        Image.Dispose();
+
+
+        return NewImageByte;
+    }
+
+    public static unsafe decimal GetDifrent(byte* imageOne, byte* imageTwo, bool UseMask, bool* mask, bool* mask2, int lenght)
+    {
+#if difrence
+        byte* testPointer = (byte*)ToTestDifrencesnInt;
+#endif
+        int pixelToCount = 0;
+        long result = 0;
+        for (int i = 0; i < lenght; i++)
+        {
+            if ((*mask) && (*mask2) && UseMask)
+            {
+                int value = (*imageOne) - (*imageTwo);
+                result += Math.Abs(value);
+                pixelToCount++;
+#if difrence
+                *testPointer = (byte)value;
+
+            }
+            else
+            {
+                *testPointer = 0;
+            }
+            testPointer++;
+#endif
+            }
+            imageOne++;
+            imageTwo++;
+            mask++;
+            mask2++;
+        }
+
+        if (pixelToCount == 0)
+        {
+            return decimal.MaxValue;
+        }
+        decimal resultDouble = result;
+        resultDouble /= pixelToCount;
+
+        return resultDouble;
+
+#if difrence
+            SaveDifrenceImage();
+
+        unsafe static void SaveDifrenceImage()
+        {
+            Bitmap bp = StreamWorking.WskaźnikNaObraz((byte*)ToTestDifrencesnInt, Width, Height);
+            bp.Save($"Framedifrence{indexDifrenceImage++}.png");
+            bp.Dispose();
+        }
+#endif
+    }
+}
---
+++ ScrollingDifferenceApp/StreamWorking.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class StreamWorking
+{
+
+    unsafe public static Bitmap WskaźnikNaObraz(byte* ws, int x, int y)
+    {
+        int lb = x * y;
+        Bitmap zw = new Bitmap(x, y);
+        BitmapData bd = zw.LockBits(new Rectangle(0, 0, x, y), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+        RGB[] LisKol = new RGB[256];
+        LisKol[0] = new RGB() { R = 0, G = 0, B = 0 };
+        for (byte i = 1; i != 0; i++)
+        {
+            LisKol[i] = new RGB() { R = i, G = i, B = i };
+        }
+        int[] l = new int[256];
+        for (int i = 0; i < y; i++)
+        {
+            RGB* tb = (RGB*)(byte*)(bd.Scan0 + i * bd.Stride);
+            for (int j = 0; j < x; j++, tb++, ws++)
+            {
+                *tb = LisKol[*ws];
+            }
+        }
+
+        zw.UnlockBits(bd);
+        return zw;
+    }
+    unsafe public static void Clear(bool* Sprawdzana, int Dłógość)
+    {
+        int ilośćDecimal = Dłógość - 16;
+        long* CzyśćDecimal = (long*)Sprawdzana;
+        int i = 0;
+        for (; i < ilośćDecimal; i += 16)
+        {
+            *CzyśćDecimal = 0;
+            CzyśćDecimal[1] = 0;
+            i += 2;
+        }
+        bool* CzyśćBool = (bool*)CzyśćDecimal;
+
+        bool* KonecBool = (bool*)Sprawdzana + Dłógość;
+        while (CzyśćBool < KonecBool)
+        {
+            *CzyśćBool = false;
+            CzyśćBool++;
+        }
+    }
+
+    public static unsafe byte* LoadMonohrome(Bitmap Obraz)
+    {
+        IntPtr mr = Marshal.AllocHGlobal(Obraz.Width * Obraz.Height);
+        StreamWorking.Clear((bool*)mr, Obraz.Width * Obraz.Height);
+        byte* obsugiwana = (byte*)mr;
+
+        int j = 0;
+        BitmapData bp = Obraz.LockBits(new Rectangle(0, 0, Obraz.Width, Obraz.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+        for (int y = 0; y < Obraz.Height; y++)
+        {
+
+            rgb* kr = (rgb*)((byte*)(bp.Scan0 + y * bp.Stride));
+            for (int x = 0; x < Obraz.Width; x++, kr++, obsugiwana++)
+            {
+                j = (*kr).r;
+                j += (*kr).g;
+                j += (*kr).b;
+                byte zw = ((byte)(j / 3));
+                *obsugiwana = zw;
+            }
+        }
+        Obraz.UnlockBits(bp);
+        return (byte*)mr;
+    }
+    public static unsafe void OtshuMethod( int WielkośćObrazu, byte* Obraz)
+    {
+        int[] his = PobierzHistogram(new Size(1, WielkośćObrazu), Obraz);
+        Progój(WielkośćObrazu, MetodaOtsu(his), Obraz);
+
+    }
+    private static unsafe int[] PobierzHistogram(Size wielkość, byte* obsugiwana)
+    {
+        int[] n = new int[256];
+        PobierzHistogram(wielkość, obsugiwana, n);
+        return n;
+    }
+
+    private static int MetodaOtsu(int[] hist)
+    {
+        byte t = 0;
+        float[] vet = new float[256];
+        vet.Initialize();
+        float PrA, PrB, p12;
+        int k;
+
+
+        PrA = hist[0];
+        PrB = Px(1, 255, hist);
+        int SrA = hist[0];
+        int SrB = ŚredniaI(1, 255, hist);
+        for (k = 1; k != 255; k++)
+        {
+            int W = hist[k];
+            PrA += W; PrB -= W;
+            SrA += W * k; SrB -= W * k;
+            p12 = PrA * PrB;
+            if (p12 < 0.0001f) p12 = 1;
+            float diff = (SrA * PrB) - (SrB * PrA);
+            vet[k] = (float)diff * diff / p12;
+        }
+        t = (byte)findMax(vet, 256);
+        return t;
+    }
+
+    private static float Px(int init, int end, int[] hist)
+    {
+        int sum = 0;
+        int i;
+        for (i = init; i <= end; i++)
+            sum += hist[i];
+
+        return (float)sum;
+    }
+
+    private static int findMax(float[] vec, int n)
+    {
+        float maxVec = 0;
+        int idx = 0;
+        int i;
+
+        for (i = 1; i < n - 1; i++)
+        {
+            if (vec[i] > maxVec)
+            {
+                maxVec = vec[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+    private static int ŚredniaI(int init, int end, int[] hist)
+    {
+        int sum = 0;
+        for (int i = init; i <= end; i++)
+            sum += i * hist[i];
+        return sum;
+    }
+    public static unsafe void PobierzHistogram(Size wielkość, byte* obsugiwana, int[] histogram)
+    {
+        int l = wielkość.Width * wielkość.Height;
+        while (l > 0)
+        {
+            histogram[*obsugiwana]++;
+            obsugiwana++;
+            l--;
+        }
+    }
+    private static unsafe bool* Progój(int WielkośćObrazu, int WartośćProgu, byte* ObrazZprogowany)
+    {
+        byte* K = ObrazZprogowany;
+        byte Prawda = 255;
+        byte Fałsz = 0;
+        for (int i = 0; i < WielkośćObrazu; i++, ObrazZprogowany++)
+        {
+            *ObrazZprogowany = (byte)((*ObrazZprogowany) > WartośćProgu ? Prawda : Fałsz);
+        }
+
+        return (bool*)K;
+    }
+
+    struct rgb
+    {
+        public byte r, g, b;
+    }
+    public struct RGB
+    {
+        public byte R, G, B;
+    }
+}
+
